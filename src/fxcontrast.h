5fe76e5836089e7a1f718a9328ea2d772fcfdaf5
Lars Muldjord <muldjordlars@gmail.com>
2018-02-03 21:50:59 +0100

    Contrast effect

---
+++ src/fxcontrast.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *            fxcontrast.h
+ *
+ *  Sat Feb 3 12:00:00 CEST 2018
+ *  Copyright 2018 Lars Muldjord
+ *  muldjordlars@gmail.com
+ ****************************************************************************/
+/*
+ *  This file is part of skyscraper.
+ *
+ *  skyscraper is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  skyscraper is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with skyscraper; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+
+#ifndef FXCONTRAST_H
+#define FXCONTRAST_H
+
+#include <QImage>
+
+#include "layer.h"
+
+class FxContrast : public QObject
+{
+  Q_OBJECT
+
+public:
+  FxContrast();
+  QImage applyEffect(QImage &image, Layer &layer);
+
+private:
+  int truncate(int value);
+
+};
+
+#endif // FXCONTRAST_H
