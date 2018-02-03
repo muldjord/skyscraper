c2ded7393a93d748ad22ea239c54ba4c04be42c5
Lars Muldjord <muldjordlars@gmail.com>
2018-02-03 17:49:16 +0100

    Stroke now works as expected

--- src/fxstroke.cpp
+++ src/fxstroke.cpp
@@ -35,45 +35,50 @@ FxStroke::FxStroke()
 QImage FxStroke::applyEffect(QImage &image, Layer &layer)
 {
   QImage buffer1(image.width() + layer.width * 2, image.height() + layer.width * 2,
-		  QImage::Format_ARGB32_Premultiplied);
+		 QImage::Format_ARGB32_Premultiplied);
   buffer1.fill(Qt::transparent);
   QPainter painter;
   painter.begin(&buffer1);
   painter.drawImage(layer.width, layer.width, image);
   painter.end();
   
-  QImage buffer2 = buffer1;
+  QImage buffer2(image.width() + layer.width * 2, image.height() + layer.width * 2,
+		 QImage::Format_ARGB32_Premultiplied);
+  buffer2.fill(Qt::transparent);
 
   QRgb *buffer1Bits = (QRgb *)buffer1.bits();
   QRgb *buffer2Bits = (QRgb *)buffer2.bits();
+
+  int width = buffer1.width();
+  int height = buffer1.height();
   
   // Now dilate each pixel layer.width number of times
   for(int a = 0; a < layer.width; ++a) {
-    for(int y = 0; y < buffer1.height(); ++y) {
-      for(int x = 0; x < buffer1.width(); ++x) {
-	int alpha = qAlpha(buffer1Bits[y * buffer1.width() + x]);
+    for(int y = 0; y < height; ++y) {
+      for(int x = 0; x < width; ++x) {
+	int alpha = qAlpha(buffer1Bits[y * width + x]);
 	if(alpha != 0) {
 	  for(int i = -1; i <= 1; ++i) {
 	    for(int j = -1; j <= 1; ++j) {
-	      if(qAlpha(buffer2Bits[(y + i) * buffer1.width() + x + j]) < alpha) {
-		buffer2Bits[(y + i) * buffer1.width() + x + j] =
-		  QColor(layer.red, layer.green, layer.blue, alpha).rgba();
+	      if(qAlpha(buffer2Bits[(y + i) * width + x + j]) < alpha) {
+		buffer2Bits[(y + i) * width + x + j] =
+		  qPremultiply(qRgba(layer.red, layer.green, layer.blue, alpha));
 	      }
 	    }
 	  }
 	}
       }
     }
-    for(int y = 0; y < buffer1.height(); ++y) {
-      for(int x = 0; x < buffer1.width(); ++x) {
-	buffer1Bits[y * buffer1.width() + x] =
-	  QColor(layer.red, layer.green, layer.blue, qAlpha(buffer2Bits[y * buffer1.width() + x])).rgba();
+    for(int y = 0; y < height; ++y) {
+      for(int x = 0; x < width; ++x) {
+	buffer1Bits[y * width + x] = buffer2Bits[y * width + x];
       }
     }
   }
 
-  painter.begin(&buffer1);
+  painter.begin(&buffer2);
   painter.drawImage(layer.width, layer.width, image);
   painter.end();
-  return buffer1;
+
+  return buffer2;
 }
