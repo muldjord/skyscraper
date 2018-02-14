9b27520dde906a0268ba6956109d4cd16b6cac09
Lars Muldjord <muldjordlars@gmail.com>
2018-02-14 16:45:38 +0100

    Skyscraper update script

---
+++ update_skyscraper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+LATEST=`curl --silent "https://api.github.com/repos/muldjord/skyscraper/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+
+if [ ! -f VERSION ]
+then
+    echo "VERSION file not found, something is wrong with your directory structure. Can't update."
+    exit
+fi
+source VERSION
+
+if [ "$LATEST" != "$VERSION" ]
+then
+    echo "--- Fetching Skyscraper v.${LATEST} ---"
+    wget https://github.com/muldjord/skyscraper/archive/${LATEST}.tar.gz
+    echo "--- Unpacking ---"
+    tar xvzf ${LATEST}.tar.gz --strip-components 1
+    echo "--- Cleaning out old build ---"
+    make clean
+    qmake
+    echo "--- Building Skyscraper v.${LATEST} ---"
+    make
+    echo "--- Installing Skyscraper v.${LATEST} ---"
+    make install
+    echo "--- Skyscraper has been updated to v.${LATEST} ---"
+else
+    echo "--- Skyscraper is already the latest release, exiting ---"
+fi
