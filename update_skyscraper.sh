0cfe9657c1cc49f66dac91fc78b3b5d67ead1188
Lars Muldjord <muldjordlars@gmail.com>
2018-02-14 17:06:10 +0100

    Added exit code checking for each command

--- update_skyscraper.sh
+++ update_skyscraper.sh
@@ -12,15 +12,35 @@ if [ "$LATEST" != "$VERSION" ]
 then
     echo "--- Fetching Skyscraper v.${LATEST} ---"
     wget https://github.com/muldjord/skyscraper/archive/${LATEST}.tar.gz
+    if [ $? != 0 ]
+    then
+	exit
+    fi
     echo "--- Unpacking ---"
     tar xvzf ${LATEST}.tar.gz --strip-components 1
+    if [ $? != 0 ]
+    then
+	exit
+    fi
     echo "--- Cleaning out old build ---"
     make clean
     qmake
+    if [ $? != 0 ]
+    then
+	exit
+    fi
     echo "--- Building Skyscraper v.${LATEST} ---"
     make
+    if [ $? != 0 ]
+    then
+	exit
+    fi
     echo "--- Installing Skyscraper v.${LATEST} ---"
     sudo make install
+    if [ $? != 0 ]
+    then
+	exit
+    fi
     echo "--- Skyscraper has been updated to v.${LATEST} ---"
 else
     echo "--- Skyscraper is already the latest release, exiting ---"
