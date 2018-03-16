2168f9a27c42e89d09798bcb54db1e7fa4b6734f
Lars Muldjord <muldjordlars@gmail.com>
2018-03-16 22:59:59 +0100

    Now clears data just because why not

--- src/netcomm.cpp
+++ src/netcomm.cpp
@@ -44,6 +44,7 @@ NetComm::~NetComm()
 void NetComm::request(QString query, QString postData)
 {
   clearAccessCache();
+  data = "";
   QUrl url(query);
   QNetworkRequest request(url);
   request.setHeader(QNetworkRequest::UserAgentHeader, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36");
