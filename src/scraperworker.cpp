c76dadb9280ddbaaa9aa7d82c184fe6f271f5fa4
Lars Muldjord <muldjordlars@gmail.com>
2018-03-25 12:53:11 +0200

    Fixed if no gameEntries was found

--- src/scraperworker.cpp
+++ src/scraperworker.cpp
@@ -348,17 +348,17 @@ GameEntry ScraperWorker::getBestEntry(const QList<GameEntry> &gameEntries,
 {
   GameEntry game;
 
+  if(gameEntries.isEmpty()) {
+    game.title = compareTitle;
+    game.found = false;
+    return game;
+  }
+
   // If scraper isn't filename search based, always return first entry
   if(config.scraper == "localdb" || config.scraper == "import" ||
      config.scraper == "arcadedb" || config.scraper == "screenscraper") {
     lowestDistance = 0;
     game = gameEntries.first();
-    return game;
-  }
-
-  if(gameEntries.isEmpty()) {
-    game.title = compareTitle;
-    game.found = false;
     return game;
   }
 
