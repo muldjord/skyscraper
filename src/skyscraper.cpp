aabdd192ca5ac2f375100a9c0205a5347aee06c8
Lars Muldjord <muldjordlars@gmail.com>
2018-12-11 20:18:09 +0100

    Now always overwrite scanline pngs

--- src/skyscraper.cpp
+++ src/skyscraper.cpp
@@ -525,6 +525,14 @@ void Skyscraper::loadConfig(const QCommandLineParser &parser)
   distro = "/usr/local/etc/skyscraper/resources/boxside.png";
   copyFile(distro, current);
 
+  current = "resources/scanlines1.png";
+  distro = "/usr/local/etc/skyscraper/resources/scanlines1.png";
+  copyFile(distro, current);
+
+  current = "resources/scanlines2.png";
+  distro = "/usr/local/etc/skyscraper/resources/scanlines2.png";
+  copyFile(distro, current);
+
   current = "dbs/README.md";
   distro = "/usr/local/etc/skyscraper/dbs/README.md";
   copyFile(distro, current);
@@ -559,14 +567,6 @@ void Skyscraper::loadConfig(const QCommandLineParser &parser)
 
   current = "resources/frameexample.png";
   distro = "/usr/local/etc/skyscraper/resources/frameexample.png";
-  copyFile(distro, current, false);
-
-  current = "resources/scanlines1.png";
-  distro = "/usr/local/etc/skyscraper/resources/scanlines1.png";
-  copyFile(distro, current, false);
-
-  current = "resources/scanlines2.png";
-  distro = "/usr/local/etc/skyscraper/resources/scanlines2.png";
   copyFile(distro, current, false);
 
   // Copy one of the example definitions.dat files if none exists
