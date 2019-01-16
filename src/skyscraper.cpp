1f1c3d01b6ddd2fff5c037e3c64f854d3e909eba
Lars Muldjord <muldjordlars@gmail.com>
2019-01-16 23:26:19 +0100

    Minor

--- src/skyscraper.cpp
+++ src/skyscraper.cpp
@@ -102,7 +102,7 @@ void Skyscraper::run()
       decProc.setReadChannel(QProcess::StandardOutput);
       decProc.start("which 7z");
       decProc.waitForFinished(10000);
-      if(decProc.readAllStandardOutput().indexOf("7z") == -1) {
+      if(!decProc.readAllStandardOutput().contains("7z")) {
 	printf("Couldn't find '7z' command. 7z is required by the '--unpack' option. On Debian derivatives such as RetroPie you can install it with 'sudo apt-get install p7zip-full'.\n\nNow quitting...\n");
 	exit(1);
       }
