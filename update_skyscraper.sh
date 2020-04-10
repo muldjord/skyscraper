022b4e2677029953ebc5014496c8d559c7e92087
Lars Muldjord <muldjordlars@gmail.com>
2020-04-10 09:29:35 +0200

    Updated

--- update_skyscraper.sh
+++ update_skyscraper.sh
@@ -8,9 +8,9 @@ then
 fi
 source VERSION
 
-if [ "$LATEST" != "$VERSION" ]
+if [ $LATEST != $VERSION ]
 then
-    echo "--- Fetching Skyscraper v.${LATEST} ---"
+    echo "--- Fetching Skyscraper v.$LATEST ---"
     wget -N https://github.com/muldjord/skyscraper/archive/${LATEST}.tar.gz
     if [ $? != 0 ]
     then
@@ -18,36 +18,43 @@ then
     fi
     echo "--- Unpacking ---"
     tar xvzf ${LATEST}.tar.gz --strip-components 1 --overwrite
+    EXITCODE=$?
     if [ $? != 0 ]
     then
 	rm VERSION
-	exit
+	echo "--- Failed to unpack Skyscraper v.${LATEST}, exiting with code $EXITCODE ---"
+	exit $EXITCODE
     fi
     rm ${LATEST}.tar.gz
     echo "--- Cleaning out old build if one exists ---"
     make clean
     rm .qmake.stash
     qmake
+    EXITCODE=$?
     if [ $? != 0 ]
     then
 	rm VERSION
-	exit
+	exit $EXITCODE
     fi
-    echo "--- Building Skyscraper v.${LATEST} ---"
-    make -j4
+    echo "--- Building Skyscraper v.$LATEST ---"
+    make -j$(nproc)
+    EXITCODE=$?
     if [ $? != 0 ]
     then
 	rm VERSION
-	exit
+	echo "--- Failed to built Skyscraper v.${LATEST}, exiting with code $EXITCODE ---"
+	exit $EXITCODE
     fi
-    echo "--- Installing Skyscraper v.${LATEST} ---"
+    echo "--- Installing Skyscraper v.$LATEST ---"
     sudo make install
+    EXITCODE=$?
     if [ $? != 0 ]
     then
 	rm VERSION
-	exit
+	echo "--- Failed to install Skyscraper v.${LATEST}, exiting with code $EXITCODE ---"
+	exit $EXITCODE
     fi
-    echo "--- Skyscraper has been updated to v.${LATEST} ---"
+    echo "--- Skyscraper has been updated to v.$LATEST ---"
 else
     echo "--- Skyscraper is already the latest version, exiting ---"
     echo "You can force a reinstall by removing the VERSION file by running 'rm VERSION'. Then rerun './update_skyscraper.sh' afterwards."
